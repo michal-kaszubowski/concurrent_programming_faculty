134e9e25252cc3efaa862e1f007a09a3584d5aec
Michal Kaszubowski <michal.mateusz.kaszubowski@gmail.com>
2023-10-19 14:42:19 +0200

    Server responding added

--- lock_files_communication/server.sh
+++ lock_files_communication/server.sh
@@ -3,7 +3,8 @@
 while true; do
     if [ -e lockfile ]; then
         dest=$(head -n 1 serverBuff.txt)
-        cont=$(tail -n +2 serverBuff.txt)
+        # cont=$(tail -n +2 serverBuff.txt)
+        read -p "Response: " $cont
         echo ">> $cont"
         echo "$cont" > "$dest"
         rm lockfile
